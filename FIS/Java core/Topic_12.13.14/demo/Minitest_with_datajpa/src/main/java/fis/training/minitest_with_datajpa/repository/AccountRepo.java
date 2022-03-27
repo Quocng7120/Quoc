8ff9874643a64354133d96e94d8173810f63c426
Quocng7120 <86445188+Quocng7120@users.noreply.github.com>
2022-03-28 00:14:59 +0700

    ...

--- FIS/Java core/Topic_12.13.14/demo/Minitest_with_datajpa/src/main/java/fis/training/minitest_with_datajpa/repository/AccountRepo.java
+++ FIS/Java core/Topic_12.13.14/demo/Minitest_with_datajpa/src/main/java/fis/training/minitest_with_datajpa/repository/AccountRepo.java
@@ -3,5 +3,8 @@ package fis.training.minitest_with_datajpa.repository;
 import fis.training.minitest_with_datajpa.entity.Account;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface AccountRepo extends CrudRepository<Account, Long> {
+    List<Account> findByAccountNumber(String accountNumber);
 }
--- FIS/Java core/Topic_12.13.14/demo/Minitest_with_datajpa/src/main/java/fis/training/minitest_with_datajpa/sevice/impl/AccountServiceImpl.java
+++ FIS/Java core/Topic_12.13.14/demo/Minitest_with_datajpa/src/main/java/fis/training/minitest_with_datajpa/sevice/impl/AccountServiceImpl.java
@@ -15,6 +15,18 @@ public class AccountServiceImpl implements AccountService {
 
     @Override
     public Account addAccount(Account account) {
+        List<Account> accountList = accountRepo.findByAccountNumber(account.getAccountNumber());
+        if (accountList.size() != 0){
+            return null;
+        }
+        if(account.getAccountNumber().length()!=12) {
+            return null;
+        }
+        if(account.getAccountName().length()<=5||account.getAccountName().length()>=100) {
+            return null;
+        }
+        account.setBalance(100000.0);
+        account.setStatus(1);
         return accountRepo.save(account);
     }
 
